35d41cef54c5beb581fe9c5ea938ac794d020395
07GAshog <113988763+07GAshog@users.noreply.github.com>
2023-01-18 11:55:42 +0000

    Finished all tests

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const { User } = require('./db');
+const bcrypt = require("bcrypt")
+
+const SALT_COUNT = 5
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -17,8 +20,44 @@ app.get('/', async (req, res, next) => {
 // POST /register
 // TODO - takes req.body of {username, password} and creates a new user with the hashed password
 
+app.post("/register", async (req, res) => {
+  try {
+    username = req.body.username
+  password = req.body.password
+
+  console.log(username, password)
+
+  const hashed = await bcrypt.hash(password, SALT_COUNT)
+  
+  const user = await User.create({username, password:hashed})
+
+  res.send(`successfully created user ${username}`).status(200)
+  } catch (error) {
+    res.send({message: "Something went wrong"}, error)
+  }
+  
+})
+
+
+
 // POST /login
 // TODO - takes req.body of {username, password}, finds user by username, and compares the password with the hashed version from the DB
 
+app.post("/login", async (req, res) => {
+  try {
+    const {username, password} = req.body
+    const possibleUser = await User.findOne({where: {username}})
+    const hashedPassword = possibleUser.password
+    const isMatching = await bcrypt.compare(password, hashedPassword)
+
+    if (possibleUser && isMatching) {
+      res.send(`successfully logged in user ${username}`).status(200)
+    } else {
+      res.send("incorrect username or password").status(401)
+    }
+  } catch (error) {
+    res.send({message: "Something went wrong"}, error).status(400)
+  }
+})
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
